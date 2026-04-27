8144f8eafc48a11bb83f307501d2a757aaaf4c83
ishanshah3 <ishanpshah3@gmail.com>
2026-04-26 21:00:41 -0400

    Update code.js

--- code.js
+++ code.js
@@ -9,3 +9,4 @@ let W2 = Math.sqrt(R * (1 - S));
 let P1 = ((S1 * (1 - R2)) / W1) / (((S1 * (1 - R2)) / W1) + ((1 - S1) * R2) / W2);
 let P2 = ((S2 * (1 - R1)) / W2) / (((S2 * (1 - R1)) / W2) + ((1 - S2) * R1) / W1);
 console.log(P1);
+console.log(P2);
