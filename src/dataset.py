c77ccee7c84885258f571e575a51bfe6b12c05ee
ishanshah3 <ishanpshah3@gmail.com>
2026-06-27 21:10:37 +0000

    Data is all good and can be filtered by surface. Need to transfer data to markov_modeling.

--- src/dataset.py
+++ src/dataset.py
@@ -1,18 +1,21 @@
 import pandas as pd
-import glob
-import os
 
-url = "https://github.com/Tennismylife/TML-Database/blob/master/2025.csv"
+url = "https://raw.githubusercontent.com/Tennismylife/TML-Database/refs/heads/master/2025.csv"
 df = pd.read_csv(url)
 
-def player_stats(df, player_name):
-    winner_df = df[df['winner_name'] == player_name]
-    loser_df = df[df['loser_name'] == player_name]
+def player_stats(df, player_name, surface="Clay"):
+    surface_df = df[df['surface'] == surface]
+    winner_df = surface_df[surface_df['winner_name'] == player_name]
+    loser_df = surface_df[surface_df['loser_name'] == player_name]
+
+    if surface_df.shape[0] == 0:
+        print(f"No data found for {player_name} on {surface}.")
+        return 0.6, 0.3
 
     if len(winner_df) == 0 and len(loser_df) == 0:
-        print(f"No data found for player: {player_name}")
+        print(f"No data found for {player_name} on {surface}.")
         return 0.6, 0.3
-    
+
     svpts_won = winner_df['w_1stWon'].sum() + winner_df['w_2ndWon'].sum() + loser_df['w_1stWon'].sum() + loser_df['w_2ndWon'].sum()
     rtpts_won = winner_df['l_svpt'].sum() - (winner_df['l_1stWon'].sum() + winner_df['l_2ndWon'].sum()) + loser_df['w_svpt'].sum() - (loser_df['w_1stWon'].sum() + loser_df['w_2ndWon'].sum())
     total_svpts = winner_df['w_svpt'].sum() + loser_df['l_svpt'].sum()
@@ -20,5 +23,5 @@ def player_stats(df, player_name):
 
     return svpts_won / total_svpts, rtpts_won / total_rtpts
 
-Jannik_Sinner_Stats = player_stats(df, "Jannik Sinner")
-print("Jannik Sinner Stats:", Jannik_Sinner_Stats)
+S_1 = player_stats(df, "Jannik Sinner")
+print("Jannik Sinner Stats:", S_1)
